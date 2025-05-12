3528512d790b1a5295451d48ca03c5dddbfb99b4
Joaqquin0 <joaquincortez2005@gmail.com>
2025-05-12 18:09:15 -0500

    feat: add schedule view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,13 @@ import { HomeComponent } from './public/pages/home/home.component';
 import { LoginComponent } from './AquaConecta/auth/presentation/pages/login/login.component';
 import { SignupComponent } from './AquaConecta/auth/presentation/pages/signup/signup.component';
 import { CreateResidentComponent } from './AquaConecta/residents/presentation/create-resident/create-resident.component';
+import {ScheduleDateComponent} from './AquaConecta/requests/components/schedule-date/schedule-date.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'requests', component: WaterRequestComponent },
+  { path: 'schedule', component: ScheduleDateComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'residents/create', component: CreateResidentComponent },
