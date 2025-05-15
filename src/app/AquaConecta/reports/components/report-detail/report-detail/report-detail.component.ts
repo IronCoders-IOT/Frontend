ff56a6493e8b271f80f1313b644483846714e544
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2025-05-14 19:10:17 -0500

    feat: add HeaderContentComponent import to report detail component

--- src/app/AquaConecta/reports/components/report-detail/report-detail/report-detail.component.ts
+++ src/app/AquaConecta/reports/components/report-detail/report-detail/report-detail.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReportdataApiService } from '../../../services/reportdata-api.service';
 import { ReportRequestEntity } from '../../../model/report-request.entity';
+import { HeaderContentComponent } from "../../../../../public/components/header-content/header-content.component";
 
 @Component({
   selector: 'app-report-detail',
   templateUrl: './report-detail.component.html',
   styleUrl: './report-detail.component.css',
   standalone: true,
+  imports: [HeaderContentComponent],
 })
 export class ReportDetailComponent implements OnInit {
   report!: ReportRequestEntity;
