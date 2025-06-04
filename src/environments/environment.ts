df34094dc03531821d62896f4ec356196bc5d1af
Joaqquin0 <joaquincortez2005@gmail.com>
2025-06-04 14:21:22 -0500

    feat(resident): add user creation, display resident by provider, and show details v3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
     production: true,
-    serverBasePath: 'https://my-json-server.typicode.com/IronCoders-IOT/fake-api-server/'
+    serverBasePath: 'http://localhost:8080/api/v1/'
 
   };
