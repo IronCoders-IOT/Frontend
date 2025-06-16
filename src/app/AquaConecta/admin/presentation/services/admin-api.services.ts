165d15b8a7f247600c42aa535e6a27f76dfafd91
Joaqquin0 <joaquincortez2005@gmail.com>
2025-06-16 11:48:22 -0500

    Updated admin-dashboard branch to display summary for admin

---
+++ src/app/AquaConecta/admin/presentation/services/admin-api.services.ts
@@ -0,0 +1,27 @@
+import {BaseService} from '../../../../shared/services/base.service';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminApiServices extends BaseService<any> {
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = `dashboard`;
+  }
+
+  getAdminSummary(): Observable<any> {
+    return this.http.get<any>(`${this.basePath}${this.resourceEndpoint}/summary`, this.httpOptions);
+  }
+
+  getAdminById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.basePath}${this.resourceEndpoint}/${id}/detail`, this.httpOptions);
+  }
+
+  updateAdmin(admin: Partial<any>): Observable<any> {
+    return this.http.put<any>(`${this.basePath}${this.resourceEndpoint}/edit`, admin, this.httpOptions);
+  }
+}
