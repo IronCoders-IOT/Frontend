ffc663fd98fc244608cb65f4f1688d7b6a2f31de
Joaqquin0 <joaquincortez2005@gmail.com>
2025-07-08 00:12:52 -0500

    fix: provide default values for serverBasePath and appName in environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    serverBasePath: import.meta.env['VITE_API_BASE_URL_DEV'],
-    appName: import.meta.env['VITE_APP_NAME']
+    serverBasePath: import.meta.env['VITE_API_BASE_URL_DEV'] || 'http://localhost:8080/api/v1/',
+    appName: import.meta.env['VITE_APP_NAME'] || 'AquaConecta'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverBasePath: import.meta.env['VITE_API_BASE_URL_PROD'],
-    appName: import.meta.env['VITE_APP_NAME']
+    serverBasePath: import.meta.env['VITE_API_BASE_URL_PROD'] || 'https://aquaconecta-gch4brewcpb5ewhc.centralus-01.azurewebsites.net/api/v1/',
+    appName: import.meta.env['VITE_APP_NAME'] || 'AquaConecta'
   };
